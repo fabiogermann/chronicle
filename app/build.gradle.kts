c7fd95d7a2a0fb4b809cc604c38a8d683effb364
Fabio Germann <fabio.germann@gmail.com>
2026-01-09 15:52:13 -0500

    chore: bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,7 +26,7 @@ android {
         minSdk = 33
         targetSdk = 36
         versionCode = 30
-        versionName = "0.60.6-SNAPSHOT"
+        versionName = "0.60.6"
 
         testInstrumentationRunner = "local.oss.chronicle.application.ChronicleTestRunner"
     }
