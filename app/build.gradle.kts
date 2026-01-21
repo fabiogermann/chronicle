83f1407f4905a7df5234a25bdf7220a0ab6613b6
fabio <fabio.germann@gmail.com>
2026-01-21 16:01:40 -0500

    ci: bump version name to 0.60.14

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,7 +26,7 @@ android {
         minSdk = 33
         targetSdk = 36
         versionCode = 35
-        versionName = "0.60.13"
+        versionName = "0.60.14"
 
         testInstrumentationRunner = "local.oss.chronicle.application.ChronicleTestRunner"
     }
