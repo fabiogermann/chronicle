e587d8e2c934f8694ec6dc5f197a91e90c6559bc
Fabio Germann <fabio.germann@gmail.com>
2026-01-26 14:02:13 -0500

    ci: bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,7 +25,7 @@ android {
         applicationId = "local.oss.chronicle"
         minSdk = 33
         targetSdk = 36
-        versionCode = 37
+        versionCode = 38
         versionName = "0.60.15"
 
         testInstrumentationRunner = "local.oss.chronicle.application.ChronicleTestRunner"
